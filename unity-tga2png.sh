0aea7d1c7d3acd0fe4397624289f981902256b92
Legwarmer1481 <109255560+Legwarmer1481@users.noreply.github.com>
2022-10-26 14:00:47 -0400

    Update unity-tga2png.sh

--- unity-tga2png.sh
+++ unity-tga2png.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#  for f in $(find -name '*.tga'); do
-#     echo "Convert ${f} to ${f%.tga}.png"
-#     ./magick "${f}" -flip "${f%.tga}.png"
-#     rename ".tga." ".png." "${f}.meta"
-#  done
+  for f in $(find -name '*.tga'); do
+     echo "Convert ${f} to ${f%.tga}.png"
+     ./magick "${f}" -flip "${f%.tga}.png"
+     rename ".tga." ".png." "${f}.meta"
+  done
 
-find . -type f -name '*.tga' -execdir ./magick "{}" -flip "{}.png" \; rename ".tga." ".tga.png." "{}.meta" \;
 
-#  echo "Tous les images de textures sont convertis!"
+
+  echo "Tous les images de textures sont convertis!"
