7b9b90bf1da33e347188756407d6fe73985f19ad
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2022-10-26 13:53:51 -0400

    Revert "Revert "Trying to fix white-space error""
    This reverts commit 4e6e34e6ca0040d6c28065dd9182d87eb1e11852.

--- unity-tga2png.sh
+++ unity-tga2png.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
- for f in $(find -name '*.tga'); do
-    echo "Convert ${f} to ${f%.tga}.png"
-    ./magick "${f}" -flip "${f%.tga}.png"
-    rename ".tga." ".png." "${f}.meta"
- done
+#  for f in $(find -name '*.tga'); do
+#     echo "Convert ${f} to ${f%.tga}.png"
+#     ./magick "${f}" -flip "${f%.tga}.png"
+#     rename ".tga." ".png." "${f}.meta"
+#  done
 
- echo "Tous les images de textures sont convertis!"
+find . -type f -name '*.tga' -execdir ./magick "{}" -flip "{}.png" \; rename ".tga." ".tga.png." "{}.meta" \;
+
+#  echo "Tous les images de textures sont convertis!"
