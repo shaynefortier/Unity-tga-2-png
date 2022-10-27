d44f2a3111790d3f1d4b1303d6814a800f84d5ed
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2022-10-26 22:06:15 -0400

    Update current directory in batch

--- unity-tga2png.bat
+++ unity-tga2png.bat
@@ -1,2 +1,2 @@
 @REM Convert using ImageMagick, rename .meta, message to tell which files are done to be able to track
-FOR /R . %%b IN ("*.tga") DO ECHO "%%b"
+FOR /R %~dp0 %%b IN ("*.tga") DO ECHO "%%b"
