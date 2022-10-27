d31fe843222d156f64bb00488fcf72e8f909e5e0
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2022-10-27 18:37:18 -0400

    Complete batch script

--- unity-tga2png.sh
+++ unity-tga2png.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -type f -name '*.tga' -exec magick "{}" -flip "{}.png" \; -exec echo {} \;
+find . -type f -name '*.tga' -exec ./tools/magick "{}" -flip "{}.png" \; -exec echo {} \;
 find . -type f -name '*.tga' -exec rename ".tga." ".tga.png." "{}.meta" \;
 
 #  echo "Tous les images de textures sont convertis!"
