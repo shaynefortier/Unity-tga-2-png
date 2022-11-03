6e3cbce51d7a3da14fa26c3ac1dfb0640f583379
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2022-11-02 20:19:33 -0400

    Fix batch does not start glitch

--- unity-tga2png.bat
+++ unity-tga2png.bat
@@ -1,9 +1,6 @@
 @ECHO off
 
-FOR /R %~dp0 %%b IN ("*.tga") DO (ECHO "%%b" &
-    "tools\convert.exe" "%%b" "%%b.png" &
-    ren "%%b".meta *.png.meta &
-    DEL "%%b")
+FOR /R %~dp0 %%b IN ("*.tga") DO (ECHO "%%b" & "tools\convert.exe" "%%b" "%%b.png" & ren "%%b".meta *.png.meta & DEL "%%b")
 
 ECHO Les convertions sont terminées
 
